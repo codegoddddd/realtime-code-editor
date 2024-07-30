bb6858d18fa6554a8ca5ea0267ca634d1765e0a8
Om Laulkar <130092463+codegoddddd@users.noreply.github.com>
2024-07-30 23:17:28 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -78,7 +78,7 @@ const Home = () => {
             <footer>
                 <h4>
                     Built with 💛 &nbsp; by &nbsp;
-                    <a href="https://github.com/codersgyan">Coder's Gyan</a>
+                    <a href="https://github.com/codegoddddd/realtime-code-editor/tree/main">Om Laulkar</a>
                 </h4>
             </footer>
         </div>
